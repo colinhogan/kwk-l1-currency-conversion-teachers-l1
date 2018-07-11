4f9bb333617b427202f3355b187892bc9804c93f
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-11 00:43:38 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -8,3 +8,29 @@ end
 def eur_to_usd(amount)
   usd=amount*(1/0.85)
 end   
+
+
+def usd_to_jpy(amount)
+  
+end 
+
+def jpy_to_usd
+  
+end
+
+
+def usd_to_gbp
+  
+end
+
+def gbp_to_usd
+  
+end
+
+def usd_to_aud
+  
+end
+
+def aud_to_usd
+  
+end
