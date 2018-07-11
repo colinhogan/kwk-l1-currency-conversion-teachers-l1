105b4d8b97b58196e02491351480030b5a44e10f
colinhogan <colin.hogan.17@tfacorps.org>
2018-07-11 00:55:02 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -34,11 +34,46 @@ def gbp_to_usd(amount)
 end
 
 def usd_to_aud(amount)
-  aud=amount*()
+  aud=amount*(1/1.35)
   return aud
 end
 
 def aud_to_usd(amount)
-  usd = amount*()
+  usd = amount*(1.35)
   return usd
 end
+
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i 
+
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+
+choice = gets.chomp().to_i
+
+case choice
+when 1 
+  usd_to_eur
+when 2 
+  usd_to_jpy
+when 3 
+  usd_to_gbp
+when 4
+  usd_to_aud
+when 5 
+  eur_to_usd
+when 6
+  jpy_to_usd
+when 7 
+  gbp_to_usd
+when 8 
+  aud_to_usd
+else puts "Invalid input, exiting..."
+end 
